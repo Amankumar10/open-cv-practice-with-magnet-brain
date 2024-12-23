a3d6bf3dfc0326eedb237bf1a7e2f76bf5e9f3a7
Aman kumar <amankumar02740@gmail.com>
2024-12-23 09:34:24 +0530

    completed  v152

--- video152.py
+++ video152.py
@@ -24,4 +24,60 @@ cv2.imshow('Our image',img)
 #Destroy
 cv2.destroyAllWindows()
 
-# 42:24
+# Limition 
+# img = cv2.imread('image/imagelow.jpg')
+# cv2.imshow("our image",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# img = cv2.imread('image/imagehigh.jpg')
+# cv2.imshow("our image",img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+# Different slicing
+# img = cv2.imread('image/imagelow.jpg')
+# img_modified=img[0:,0:200]
+# cv2.imshow("our image",img_modified)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# display ndarray
+
+# # Black image
+import numpy as np
+# matrx = np.zeros((500,500),dtype='uint8')
+# cv2.imshow("our image",matrx)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# import random
+# matrx = np.zeros((800,800,3),dtype='uint8')
+
+
+# for i in range(800):
+#     for j in range(800):
+#         matrx[i,j]=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+        
+        
+# cv2.imshow("our image",matrx)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+# import random
+# matrx = np.zeros((800,800,3),dtype='uint8')
+
+
+# for i in range(800):
+#     for j in range(800):
+#         matrx[i,j]=[random.randint(0,100),random.randint(0,100),random.randint(0,100)]
+        
+        
+# cv2.imshow("our image",matrx)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
